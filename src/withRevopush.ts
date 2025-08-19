841f37beba62095d172ce0bfac7e609fcacea720
kit2o <kitwooki2o@gmail.com>
2025-08-19 15:24:47 +0100

    init expo plugin version

---
+++ src/withRevopush.ts
@@ -0,0 +1,184 @@
+import {
+    type ConfigPlugin,
+    AndroidConfig,
+    withInfoPlist,
+    withAppDelegate,
+    withStringsXml,
+    withAppBuildGradle,
+    withMainApplication,
+} from '@expo/config-plugins';
+import {addImports} from '@expo/config-plugins/build/android/codeMod';
+import {mergeContents} from '@expo/config-plugins/build/utils/generateCode';
+
+type RevopushConfig = {
+    ios?: {
+        CodePushServerUrl?: string;
+        CodePushDeploymentKey: string;
+    };
+    android?: {
+        CodePushServerUrl?: string;
+        CodePushDeploymentKey: string;
+    };
+}
+
+const withIosPlugin: ConfigPlugin<RevopushConfig> = (config, {ios}) => {
+    if (!ios) {
+        return config;
+    }
+
+    config = withInfoPlist(config, (config) => {
+        if (!ios.CodePushDeploymentKey) {
+            throw new Error('Missing iOS CodePushDeploymentKey');
+        }
+
+        config.modResults.CodePushDeploymentKey = ios.CodePushDeploymentKey;
+
+        if (ios.CodePushServerUrl) {
+            config.modResults.CodePushServerURL = ios.CodePushServerUrl;
+        }
+
+        return config;
+    });
+
+    config = withAppDelegate(config, (config) => {
+        const {modResults} = config;
+        const {language} = modResults;
+
+        switch (language) {
+            case 'swift': {
+                config.modResults.contents = mergeContents({
+                    src: modResults.contents,
+                    comment: '//',
+                    tag: 'revopush-updates-header',
+                    offset: 1,
+                    anchor: /import React/,
+                    newSrc: 'import CodePush',
+                }).contents;
+
+                config.modResults.contents = config.modResults.contents.replace(
+                    /return Bundle\.main\.url\(forResource: "main", withExtension: "jsbundle"\)/,
+                    `return CodePush.bundleURL()`,
+                );
+                break;
+            }
+            default: {
+                throw new Error(`Revopush plugin doesn't support language: ${language}`);
+            }
+        }
+
+        return config;
+    });
+
+    return config;
+};
+
+const withAndroidPlugin: ConfigPlugin<RevopushConfig> = (config, {android}) => {
+    if (!android) {
+        return config;
+    }
+
+    config = withStringsXml(config, config => {
+        if (!android.CodePushDeploymentKey) {
+            throw new Error('Missing Android CodePushDeploymentKey');
+        }
+
+        AndroidConfig.Strings.setStringItem(
+            [
+                {
+                    $: {
+                        name: 'CodePushDeploymentKey',
+                        translatable: 'false',
+                    },
+                    _: android.CodePushDeploymentKey,
+                },
+            ],
+            config.modResults,
+        );
+
+        if (android.CodePushServerUrl) {
+            AndroidConfig.Strings.setStringItem(
+                [
+                    {
+                        $: {
+                            name: 'CodePushServerUrl',
+                            translatable: 'false',
+                        },
+                        _: android.CodePushServerUrl,
+                    },
+                ],
+                config.modResults,
+            );
+        }
+
+        return config;
+    });
+
+    config = withAppBuildGradle(config, (config) => {
+        if (config.modResults.language !== 'groovy') {
+            throw new Error(`Cannot modify build.gradle if it's not groovy`);
+        }
+
+        if (!config.modResults.contents.includes('@revopush/gradle')) {
+            config.modResults.contents =
+                config.modResults.contents +
+                '\n' +
+                '// @revopush/gradle' +
+                '\n' +
+                'apply from: "../../node_modules/@revopush/react-native-code-push/android/codepush.gradle"' +
+                '\n';
+        }
+
+        return config;
+    });
+
+    config = withMainApplication(config, (config) => {
+        const {modResults} = config;
+        const {language} = modResults;
+
+        config.modResults.contents = addImports(
+            modResults.contents,
+            ['com.microsoft.codepush.react.CodePush'],
+            language === 'java',
+        );
+
+        if (language === 'kt') {
+            config.modResults.contents = mergeContents({
+                src: modResults.contents,
+                comment: '//',
+                tag: '@revopush/main-application-kt',
+                offset: 1,
+                anchor: /override fun getUseDeveloperSupport\(\): Boolean = BuildConfig\.DEBUG/,
+                newSrc: `          override fun getJSBundleFile(): String {
+              return CodePush.getJSBundleFile()
+          }`,
+            }).contents;
+
+            config.modResults.contents = mergeContents({
+                src: modResults.contents,
+                comment: '//',
+                tag: '@revopush/main-application-kt-oncreate',
+                offset: 1,
+                anchor: /super\.onCreate\(\)/,
+                newSrc: `super.onCreate()
+
+    try {
+        val deploymentKey = getString(R.string.CodePushDeploymentKey)
+        CodePush.getInstance(deploymentKey, this, BuildConfig.DEBUG)
+    } catch (e: Exception) {}`,
+            }).contents;
+
+            return config;
+        }
+
+        throw new Error(`Cannot modify MainApplication because the language "${language}" is not supported`);
+    });
+
+    return config;
+}
+
+const withRevopushPlugin: ConfigPlugin<RevopushConfig> = (config, options = {}) => {
+    config = withAndroidPlugin(config, options);
+    return withIosPlugin(config, options);
+};
+
+export default withRevopushPlugin
